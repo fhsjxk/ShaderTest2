481aa3e4cb102bf514968014d2ff4b16c49e01e8
fhsjxk <3398028530@qq.com>
2026-03-16 18:29:19 +0800

    Changes

--- lib/atmosphere__.glsl
+++ lib/atmosphere__.glsl
@@ -18,26 +18,26 @@ const float PLANET_RADIUS       = 6371.0;
 const float ATM_THICKNESS       = 100.0;
 const float ATM_RADIUS          = PLANET_RADIUS + ATM_THICKNESS;
 
-const vec4 SUN_RADIANCE         = vec4(1.68, 1.83, 1.99, 1.31);
+const vec3 SUN_RADIANCE         = vec3(1.1123, 0.975098, 0.912109);
 
-const vec4 RAYLEIGH_SCAT_BASE   = vec4(6.605e-3, 1.067e-2, 1.842e-2, 3.156e-2) * 1.5;
+const vec3 RAYLEIGH_SCAT_BASE   = vec3(5.8e-6, 13.5e-6, 33.1e-6) * 1.5e3;
 
-const vec4 OZONE_ABS_BASE       = vec4(3.472e-21, 3.914e-21, 1.349e-21, 11.03e-23) * 1e-4f;
+const vec3 OZONE_ABS_BASE       = vec3(0.650e-6, 1.881e-6, 0.085e-6)*0.0;
 
-const vec4 AEROSOL_ABS_BASE     = vec4(1.0e-22);
-const vec4 AEROSOL_SCAT_BASE    = vec4(1.5e-22);
+const vec3 AEROSOL_ABS_BASE     = vec3(1.0e-22);
+const vec3 AEROSOL_SCAT_BASE    = vec3(1.5e-22);
 
 const float AEROSOL_HEIGHT_SCALE = 1.2;
 const float AEROSOL_TURBIDITY    = 1.2;
 const float AEROSOL_BASE_DENSITY = 1.37e20;
 
-const vec4 GROUND_ALBEDO         = vec4(0.3);
+const vec3 GROUND_ALBEDO         = vec3(0.3);
 
-vec4 transmittanceFromLUT(float cosTheta, float normalizedAlt)
+vec3 transmittanceFromLUT(float cosTheta, float normalizedAlt)
 {
     vec2 uv = vec2(clamp(cosTheta * 0.5 + 0.5, 0.0, 1.0),
                    clamp(normalizedAlt,      0.0, 1.0));
-    return texture({{RT_TRANSMIT_LUT}}, uv).rgba;
+    return texture({{RT_TRANSMIT_LUT}}, uv).rgb;
 }
 
 float raySphereIntersect(vec3 origin, vec3 dir, float radius)
@@ -72,11 +72,11 @@ float rayleighPhase(float cosTheta)
 }
 
 void getAtmCoefficients(float h,
-                        out vec4 aerosolAbs,
-                        out vec4 aerosolScat,
-                        out vec4 molecularAbs,
-                        out vec4 molecularScat,
-                        out vec4 extinction)
+                        out vec3 aerosolAbs,
+                        out vec3 aerosolScat,
+                        out vec3 molecularAbs,
+                        out vec3 molecularScat,
+                        out vec3 extinction)
 {
     h = max(h, 0.0);
 
@@ -90,44 +90,44 @@ void getAtmCoefficients(float h,
     float t = log(h + 1e-4) - 3.22261;
     float ozoneDensity = 3.78547397e20 / (h + 1e-4) * exp(-t * t * 5.55555555);
     molecularAbs = OZONE_ABS_BASE * 300.0 * ozoneDensity;
-    molecularAbs += 1e-3 * exp(-0.07771971 * pow(h + 1.0, 1.16364243));
+    molecularAbs += 1e-2 * exp(-0.07771971 * pow(h + 1.0, 1.16364243));
 
     extinction = aerosolAbs + aerosolScat + molecularAbs + molecularScat;
 }
 
-vec4 multiScatteringIsotropic(float cosTheta, float normalizedAlt, float r)
+vec3 multiScatteringIsotropic(float cosTheta, float normalizedAlt, float r)
 {
-    //return vec4(0);
+    //return vec3(0);
     float solidAngle = 2.0 * PI * (1.0 - sqrt(max(0.0, r*r - PLANET_RADIUS*PLANET_RADIUS)) / r);
-    vec4 transToGround = transmittanceFromLUT(cosTheta, 0.0);
-    vec4 transGroundToSample = transmittanceFromLUT(1.0, 0.0) / transmittanceFromLUT(1.0, normalizedAlt);
+    vec3 transToGround = transmittanceFromLUT(cosTheta, 0.0);
+    vec3 transGroundToSample = transmittanceFromLUT(1.0, 0.0) / transmittanceFromLUT(1.0, normalizedAlt);
 
-    vec4 groundRadiance = (INV_4PI * solidAngle) *
+    vec3 groundRadiance = (INV_4PI * solidAngle) *
                           (GROUND_ALBEDO / PI) *
                           transToGround * transGroundToSample *
                           max(0.0, cosTheta);
 
-    vec4 approxMulti = 0.015 * vec4(0.217, 0.347, 0.594, 1.0) /
-                       (1.0 + 5.0 * exp(-17.92 * cosTheta)) * 0.7;
+    vec3 approxMulti = 0.015 * vec3(0.217, 0.347, 0.7) /
+                       (1.0 + 5.0 * exp(-17.92 * cosTheta));
 
     return groundRadiance + approxMulti;
 }
 
-vec4 multiScatteringAnisotropic(float cosTheta, float h)
+vec3 multiScatteringAnisotropic(float cosTheta, float h)
 {
     //float phase = mix(hgPhase(cosTheta, 0.45),mix(hgPhase(cosTheta, 0.75), hgPhase(cosTheta, 0.95), 0.02), 0.3);
     float phase = mix(hgPhase(cosTheta, 0.6), hgPhase(cosTheta, 0.95), 0.03);
-    return RAYLEIGH_SCAT_BASE * phase * AEROSOL_TURBIDITY * AEROSOL_BASE_DENSITY * exp(-h / AEROSOL_HEIGHT_SCALE) * 2e-19;
+    return RAYLEIGH_SCAT_BASE * phase * AEROSOL_TURBIDITY * AEROSOL_BASE_DENSITY * exp(-h / AEROSOL_HEIGHT_SCALE) * 3e-19;
 }
 
-vec4 computeTransmittance(vec3 origin, vec3 rayDir)
+vec3 computeTransmittance(vec3 origin, vec3 rayDir)
 {
     float rayLen = raySphereIntersect(origin, rayDir, ATM_RADIUS);
     if (rayLen < 0.0)
-        return vec4(1.0);
+        return vec3(1.0);
 
     float dt = rayLen / float(TRANSMITTANCE_STEPS);
-    vec4 opticalDepth = vec4(0.0);
+    vec3 opticalDepth = vec3(0.0);
 
     for (int i = 0; i < TRANSMITTANCE_STEPS; ++i)
     {
@@ -135,9 +135,9 @@ vec4 computeTransmittance(vec3 origin, vec3 rayDir)
         vec3 p = origin + rayDir * t;
         float h = length(p) - PLANET_RADIUS;
 
-        vec4 aerosolAbs, aerosolScat;
-        vec4 molAbs, molScat;
-        vec4 extinction;
+        vec3 aerosolAbs, aerosolScat;
+        vec3 molAbs, molScat;
+        vec3 extinction;
 
         getAtmCoefficients(h,aerosolAbs, aerosolScat,molAbs,molScat, extinction);
 
@@ -147,7 +147,7 @@ vec4 computeTransmittance(vec3 origin, vec3 rayDir)
     return exp(-opticalDepth);
 }
 
-vec4 computeTransmittanceLUT(vec2 uv)
+vec3 computeTransmittanceLUT(vec2 uv)
 {
     float cosTheta = uv.x * 2.0 - 1.0;
     vec3 rayDir = vec3(sqrt(max(0.0, 1.0 - cosTheta * cosTheta)),0.0,cosTheta);
@@ -159,7 +159,7 @@ vec4 computeTransmittanceLUT(vec2 uv)
 }
 
 
-vec4 computeInscattering(vec3 rayDir)
+vec3 computeInscattering(vec3 rayDir)
 {
     vec3 rayOrigin = vec3(0.0, 0.0, PLANET_RADIUS + max((eyeAltitude - 64.0) * 0.05, 0.01));
     vec3 sunDir    = normalize(sunDirection.xzy);
@@ -185,12 +185,12 @@ vec4 computeInscattering(vec3 rayDir)
         rayLen = (groundDist > 0.0) ? (groundDist - atmosDist) : secondAtmos;
     }
 
-    if (rayLen <= 0.0) return vec4(0.0);
+    if (rayLen <= 0.0) return vec3(0.0);
 
     float dt = rayLen / float(INSCATTERING_STEPS);
 
-    vec4 L = vec4(0.0);
-    vec4 T = vec4(1.0);
+    vec3 L = vec3(0.0);
+    vec3 T = vec3(1.0);
 
     float rayleighPhase = rayleighPhase(-cosTheta);
     float aerosolPhase  = aerosolPhase(-cosTheta);
@@ -206,21 +206,21 @@ vec4 computeInscattering(vec3 rayDir)
 
         float sunCosTheta = dot(p / r, sunDir);
 
-        vec4 aerosolAbs, aerosolScat, molAbs, molScat, ext;
+        vec3 aerosolAbs, aerosolScat, molAbs, molScat, ext;
         getAtmCoefficients(h, aerosolAbs, aerosolScat, molAbs, molScat, ext);
 
-        vec4 transToSun = transmittanceFromLUT(sunCosTheta, normalizedH);
+        vec3 transToSun = transmittanceFromLUT(sunCosTheta, normalizedH);
 
-        vec4 multiIso = multiScatteringIsotropic(sunCosTheta, normalizedH, r);
-        vec4 multiAni = multiScatteringAnisotropic(-cosTheta, h) * transToSun;
+        vec3 multiIso = multiScatteringIsotropic(sunCosTheta, normalizedH, r);
+        vec3 multiAni = multiScatteringAnisotropic(-cosTheta, h) * transToSun;
 
-        vec4 singleScat = (molScat * rayleighPhase + aerosolScat * aerosolPhase) * transToSun;
-        vec4 multiScat  = (multiIso + multiAni) * (molScat + aerosolScat);
+        vec3 singleScat = (molScat * rayleighPhase + aerosolScat * aerosolPhase) * transToSun;
+        vec3 multiScat  = (multiIso + multiAni) * (molScat + aerosolScat);
 
-        vec4 source = SUN_RADIANCE * (singleScat + multiScat);
+        vec3 source = SUN_RADIANCE * (singleScat + multiScat);
 
-        vec4 stepT = exp(-ext * dt);
-        vec4 integrated = (source - source * stepT) / max(ext, vec4(1e-6));
+        vec3 stepT = exp(-ext * dt);
+        vec3 integrated = (source - source * stepT) / max(ext, vec3(1e-6));
 
         L += T * integrated;
         T *= stepT;
@@ -229,17 +229,17 @@ vec4 computeInscattering(vec3 rayDir)
     float sun = float(cosTheta > 0.99999) * 15000.0;
     //L += T * SUN_RADIANCE * sun;
 
-    return L;
+    return L * 10.0;
 }
 
-const mat4x3 M = mat4x3(
-    137.672389239975, -8.632904716299537, -1.7181567391931372,
-    32.549094028629234, 91.29801417199785, -12.005406444382531,
-    -38.91428392614275, 34.31665471469816, 29.89044807197628,
-    8.572844237945445, -11.103384660054624, 117.47585277566478
-);
-
-vec3 RgbFromSpectral(vec4 L)
-{
-    return M * L * 0.15;
-}
+//const mat4x3 M = mat4x3(
+//    137.672389239975, -8.632904716299537, -1.7181567391931372,
+//    32.549094028629234, 91.29801417199785, -12.005406444382531,
+//    -38.91428392614275, 34.31665471469816, 29.89044807197628,
+//    8.572844237945445, -11.103384660054624, 117.47585277566478
+//);
+//
+//vec3 RgbFromSpectral(vec3 L)
+//{
+//    return M * L * 0.05;
+//}
--- program/prepare_transmitLUT.glsl
+++ program/prepare_transmitLUT.glsl
@@ -9,7 +9,7 @@ layout(location = 0) out vec4 outColor;
 
 void main()
 {
-    outColor = computeTransmittanceLUT(texcoord);
+    outColor.rgba = computeTransmittanceLUT(texcoord);
 }
 #endif
 
